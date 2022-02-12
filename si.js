ae0e23e5c8b80f880a5ee847dd7ef920be67dec6
Enzo Coquelle <enzo.coquelle06@gmail.com>
2022-02-12 10:08:25 +0100

    testing nanospinner

--- si.js
+++ si.js
@@ -1,4 +1,5 @@
 const si = require('systeminformation');
+const { createSpinner } = require('nanospinner');
 
 async function run() {
 	console.log(await si.system());
@@ -7,6 +8,18 @@ async function run() {
 	console.log(await si.chassis());
 	console.log(await si.osInfo());
 	console.log(await si.versions("*"));
+
+	const spinner = createSpinner('Run test').start()
+
+	setTimeout(() => {
+		spinner.update({
+			text: 'Modified',
+			color: 'cyan',
+		});
+		setTimeout(() => {
+			spinner.stop({ text: 'Done!', mark: ':O', color: 'magenta' })
+		}, 2000)
+	}, 1000)
 }
 
 run();
